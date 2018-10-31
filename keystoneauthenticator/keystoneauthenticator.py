3c1311b4560199d71def6c893d023af9ef8aa5f2
Jason <jasonanderson@uchicago.edu>
2018-10-31 16:50:59 -0500

    Release jupyterlab_swift
    Includes default project selection. Pins region to UC, that's where the
    containers are living.

--- keystoneauthenticator/keystoneauthenticator.py
+++ keystoneauthenticator/keystoneauthenticator.py
@@ -3,6 +3,7 @@ from keystoneauth1 import session
 from keystoneauth1.exceptions.http import Unauthorized
 from keystoneauth1.identity import v3
 from tornado import gen
+from traceback import format_exc
 from traitlets import Unicode
 
 class KeystoneAuthenticator(Authenticator):
@@ -22,7 +23,6 @@ class KeystoneAuthenticator(Authenticator):
                            username=username,
                            password=password,
                            user_domain_name='default',
-                           project_domain_name='default',
                            unscoped=True)
         sess = session.Session(auth=auth)
 
@@ -31,10 +31,24 @@ class KeystoneAuthenticator(Authenticator):
         except Unauthorized:
             return None
 
+        try:
+            project_response = sess.get('{}/auth/projects'.format(self.auth_url))
+            projects = project_response.json()['projects']
+            projects = [p for p in projects if p['enabled'] and p['name'] != 'openstack']
+        except Exception as exc:
+            self.log.error('Failed to get project list for user {}'.format(username))
+            self.log.debug(format_exc())
+            projects = []
+
         userdict = {'name': username}
         userdict['auth_state'] = auth_state = {}
         auth_state['auth_url'] = self.auth_url
         auth_state['os_token'] = token
+
+        if projects:
+            auth_state['project_name'] = projects[0]['name']
+        else:
+            self.log.warn('Could not select default project for user {}'.format(username))
 
         return userdict
 
@@ -46,3 +60,9 @@ class KeystoneAuthenticator(Authenticator):
             return
         spawner.environment['OS_AUTH_URL'] = auth_state['auth_url']
         spawner.environment['OS_TOKEN'] = auth_state['os_token']
+        spawner.environment['OS_AUTH_TYPE'] = 'token'
+        spawner.environment['OS_IDENTITY_API_VERSION'] = '3'
+
+        project_name = auth_state.get('project_name')
+        if project_name:
+            spawner.environment['OS_PROJECT_NAME'] = project_name
